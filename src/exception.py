898208e28a9072e5ccd95816caec7670980d4e54
Zeyad Khalaf <zkhalaf404@gmail.com>
2024-06-25 06:36:37 +0300

    Add notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,6 @@
 import sys
 import logging
-import os
-from datetime import datetime
-
-# Create the log filename with a timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
-# Define the directory for the logs
-logs_dir = os.path.join(os.getcwd(), "logs")
-
-# Create the directory if it doesn't exist
-os.makedirs(logs_dir, exist_ok=True)
-
-# Define the full path for the log file
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
-
-# Configure the logging module
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -38,9 +18,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=="__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.error("An error occurred: %s", str(CustomException(e, sys)))
-        raise CustomException(e, sys)
+
